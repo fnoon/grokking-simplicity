a31e9b0c3dff5f9e9da735fe64b1829f3ce53e75
Frederick Noon <fnoon@proton.me>
2024-07-15 21:52:32 -0700

    Code after Chapter 4: extracted calculations

--- shopping.go
+++ shopping.go
@@ -9,28 +9,47 @@ type sku struct {
 	price int
 }
 
-var cart []sku
-
-var cart_total int
+var (
+	cart       []sku
+	cart_total int
+)
 
 func add_item_to_cart(name string, price int) {
-	item := sku{name, price}
-	cart = append(cart, item)
+	cart = add_item(cart, name, price)
 	calc_cart_total()
 }
 
+// pure function ("calculation")
+func add_item(cart []sku, name string, price int) []sku {
+	item := sku{name, price}
+	cart = dup_sku_slice(cart) // enforce COW: don't mutate inputs
+	cart = append(cart, item)
+	return cart
+}
+
+// pure function ("calculation")
+func dup_sku_slice(src []sku) []sku {
+	if src == nil {
+		return nil
+	}
+	dst := make([]sku, len(src))
+	_ = copy(dst, src)
+	return dst
+}
+
 func calc_cart_total() {
-	calc_total()
+	cart_total = calc_total(cart)
 	update_shipping_icons()
 	update_tax_dom()
 }
 
-func calc_total() {
+// pure function ("calculation")
+func calc_total(cart []sku) int {
 	total := 0
 	for _, sku := range cart {
 		total += sku.price
 	}
-	cart_total = total
+	return total
 }
 
 type buy_button struct {
@@ -55,7 +74,7 @@ func update_shipping_icons() {
 	buttons := get_buy_buttons_dom()
 	for _, button := range buttons {
 		sku := button.item
-		if sku.price+cart_total >= 20 {
+		if gets_free_shipping(cart_total, sku) {
 			show_free_shipping_icon(button)
 		} else {
 			hide_free_shipping_icon(button)
@@ -63,11 +82,21 @@ func update_shipping_icons() {
 	}
 }
 
-func set_tax_dom(tax float64) {
+// pure function ("calculation")
+func gets_free_shipping(cart_total int, item sku) bool {
+	return cart_total+item.price >= 20
 }
 
 func update_tax_dom() {
-	set_tax_dom(float64(cart_total) * 0.10)
+	set_tax_dom(calculate_tax(cart_total))
+}
+
+func set_tax_dom(tax float64) {
+}
+
+// pure function ("calculation")
+func calculate_tax(price int) float64 {
+	return float64(price) * 0.10
 }
 
 func main() {
